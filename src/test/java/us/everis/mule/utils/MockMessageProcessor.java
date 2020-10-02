4dd6bd0ba63ed1f78ae2e98218b2527f0dca7ba5
evelynda1985 <eagredo@mum.edu>
2020-10-01 22:40:05 -0400

    first commit

---
+++ src/test/java/us/everis/mule/utils/MockMessageProcessor.java
@@ -0,0 +1,15 @@
+package us.everis.mule.utils;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.processor.MessageProcessor;
+
+public class MockMessageProcessor implements MessageProcessor {
+
+	@Override
+	public MuleEvent process(MuleEvent event) throws MuleException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
